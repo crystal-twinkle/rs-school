1fe2ca7654e9fd8165e2d8cc39422d737ec45c00
Kristina <nice.kres@mail.ru>
2023-06-26 15:59:42 +0300

    feat: implement webpack and eslint

---
+++ RSS-CSS-Selectors/webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const {merge} = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const EslintPlugin = require('eslint-webpack-plugin');
+
+
+const baseConfig = {
+  entry: path.resolve(__dirname, './src/index'),
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.ts$/i,
+        use: 'ts-loader',
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/,
+        use: ['file-loader'],
+      },
+      {
+        test: /\.svg$/,
+        use: ['raw-loader'],
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.ts'],
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, './dist'),
+      publicPath: '/dist/',
+      writeToDisk: true,
+    },
+  },
+  output: {
+    filename: 'indexFull.js',
+    path: path.resolve(__dirname, './dist'),
+  },
+  plugins: [
+    new EslintPlugin({extensions: ['ts']}),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/index.html'),
+      filename: 'index.html',
+    }),
+    new CleanWebpackPlugin({}),
+  ],
+};
+
+module.exports = ({mode}) => {
+  const isProductionMode = mode === 'prod';
+  const envConfig = isProductionMode ? require('./webpack.prod.config') : require('./webpack.dev.config');
+
+  return merge(baseConfig, envConfig);
+};
